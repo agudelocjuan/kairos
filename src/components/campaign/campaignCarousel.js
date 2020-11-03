60b6418b55ba78f27912cde45bf1e83e3102eb1c
Tucker <tuckerschoos@gmail.com>
2020-11-03 13:30:58 -0500

    carousel

--- src/components/campaign/campaignCarousel.js
+++ src/components/campaign/campaignCarousel.js
@@ -1,25 +1,42 @@
 import React from "react"
 import { Container, Row, Col } from "reactstrap"
 import Flickity from "react-flickity-component"
+import YouTube from "react-youtube"
 
 const CampaignCarousel = () => {
   const options = {
     draggable: true,
-    initialIndex: 0,
     wrapAround: true,
-    pageDots: false,
-    adaptiveHeight: true,
+    pageDots: true,
+    adaptiveHeight: false,
     selectedAttraction: 0.2,
     friction: 0.8,
-    fade: true,
+    fade: false,
     prevNextButtons: false,
   }
   const slides = [
     {
+      type: "quote",
+      quote:
+        "There were literally tenants crying tears of gratitude at City Hall but real estate investor associations were also in support,",
+      source: "PG Sittenfeld, Cincinnati City Council, NBC article",
+    },
+    {
       type: "video",
+      videoId: "u57uy6gKSlw",
+      embed:
+        "https://www.youtube.com/watch?v=u57uy6gKSlw&feature=youtu.be&ab_channel=TeamKairos",
     },
     {
       type: "quote",
+      quote:
+        "By working with city governments to make onerous security deposits optional, Rhino wants to eliminate a barrier to affordable housing",
+      source: "Curbed",
+    },
+    {
+      type: "video",
+      embed: "https://www.youtube.com/watch?v=DzymrHY-T-c&feature=youtu.be",
+      videoId: "DzymrHY-T-c",
     },
   ]
   return (
@@ -30,20 +47,27 @@ const CampaignCarousel = () => {
         </Col>
       </Row>
       <Row className="d-flex justify-content-center">
-        <Col md="8">
+        <Col md="12">
           <Flickity className={"carousel mb-4"} options={options}>
             {slides.map((i, idx) => {
               const quoteBlock = (
-                <div key={idx} className="w-75">
-                  <p className="mt-2 mb-1">{i.type}</p>
+                <div key={idx} className="slide is-quote">
+                  <div className="title navigational-graffiti">{i.quote}</div>
+                  <p>– {i.source}</p>
                 </div>
               )
               const videoBlock = (
-                <div key={idx} className="w-75">
-                  <p className="mt-2 mb-1">{i.type}</p>
+                <div key={idx} className="slide is-video">
+                  <div className="v-container">
+                    <YouTube videoId={i.videoId} />
+                  </div>
                 </div>
               )
-              return i.type === "quote" ? quoteBlock : videoBlock
+              return (
+                <div className="carousel--inner" key={idx}>
+                  {i.type === "quote" ? quoteBlock : videoBlock}
+                </div>
+              )
             })}
           </Flickity>
         </Col>

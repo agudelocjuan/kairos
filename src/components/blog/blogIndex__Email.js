c79c30fb7fb8d81ff127e1a2cdb9e1f73a5a68c0
Prabhav Khandelwal <prabhav@alright.studio>
2020-12-11 15:15:53 -0500

    email signup

--- src/components/blog/blogIndex__Email.js
+++ src/components/blog/blogIndex__Email.js
@@ -3,16 +3,24 @@ import { Link } from "gatsby"
 import { connect } from "react-redux"
 import { Container, Row, Col } from "reactstrap"
 
+import Email from "../global/email--blog"
+
 import img_1 from "../../images/about/about_1.png"
 import img_2 from "../../images/about/about_2.png"
 import arrow from "../../images/icons/arrow-diag-red.svg"
 
 const blogIndex__Email = ({ mobile }) => {
   return (
-    <Container fluid id="blogIndex__Email" className={`bg-white`}>
+    <Container fluid id="blogIndex__Email" className="">
       <Row id="">
-        <Col md={{ size: 6, offset: 1 }} className="left-container">
-            <h1>Email Capture</h1>
+        <Col md="4" className="left-container text">
+            <h3 className="title">Want more Kairos?</h3>
+            <p>Sign up for our newsletter today!</p>
+            
+        </Col>
+        
+        <Col md="8" className="left-container">
+            <Email />
             
         </Col>
       </Row>

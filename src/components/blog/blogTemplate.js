82465171a1a01bd90fb0d3a979d2212af080f31a
Prabhav Khandelwal <prabhav@alright.studio>
2021-01-21 11:02:32 -0500

    conditionally loading window

--- src/components/blog/blogTemplate.js
+++ src/components/blog/blogTemplate.js
@@ -64,8 +64,8 @@ class BlogTemplate extends React.Component {
     }
 
     // for twitter share button
-    // const shareItem = typeof window !== "undefined" ? window.location : ""
-    const shareItem = window.location.href
+    const shareItem = typeof window !== "undefined" ? window.location.href : ""
+    // const shareItem = window.location.href
 
     // this is where all the post content is contained
     console.log(shareItem)
